2761db213b32b28c01b0ace2c39be562765ef9bb
victor.bertoldo <eu_viktor@hotmail.com>
2020-01-27 13:23:05 -0300

    Testes com operadores e entrada de dados

---
+++ Testes/entrada_dados.py
@@ -0,0 +1,9 @@
+
+Nome = str(input('Digite seu nome:'))
+Idade = int(input('Digite sua idade:'))
+
+print(Nome)
+print(Idade)
+
+print('Tipo variavel Nome:',type(Nome))
+print('Tipo variavel Idade:',type(Idade))
---
+++ Testes/teste_operadores.py
@@ -0,0 +1,16 @@
+
+n1 = 2
+n2 = 5
+soma = n1+n2
+subtracao = n1-n2
+multplicacao = n1*n2
+divisao = n1/n2
+exponenciacao = n1**n2
+modulo = n1%n2 #resto da divisão
+
+print(f'soma:{soma}')
+print(f'subtracao:{subtracao}')
+print(f'multplicacao:{multplicacao}')
+print(f'divisao:{divisao}')
+print(f'exponenciacao:{exponenciacao}')
+print(f'modulo:{modulo}')
