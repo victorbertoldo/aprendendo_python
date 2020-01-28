bb28568fa60714d19cc7e5ef854995544cd981fa
victor.bertoldo <eu_viktor@hotmail.com>
2020-01-28 10:09:01 -0300

    Testes com condicionais e repetição

---
+++ Testes/teste_for.py
@@ -0,0 +1,10 @@
+x = [[1,2,3], [5,4,6], ['assd','dsds','vcvc']]
+for i in x:
+    print(i)
+
+
+for i in range(1,1001):
+    print(i)
+
+for i in range(1000,0,-1):
+    print(i)
---
+++ Testes/teste_tabuada.py
@@ -0,0 +1,6 @@
+
+for i in range(0,11):
+    print('*' * 15)
+    for x in range(0,11):
+        a = i * x
+        print(i,' x ',x,' = ',a)
